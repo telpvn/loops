7addc52d91eea3ebb0f0e689467bcb48f05ccbb0
telpvn <telpvn@mail.ru>
2024-04-24 09:27:40 +0400

    Regular for loop continue 1

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
+            System.out.println("Hello");
             if (i == 3) {
                 continue;
             }
-            System.out.println("Hello");
         }
     }
 }
