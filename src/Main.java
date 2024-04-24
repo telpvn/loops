9c3d294c8c96bcafc1ed21a5fa8af5df8fac899c
telpvn <telpvn@mail.ru>
2024-04-24 14:31:30 +0400

    do while 1

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean isContinue = true;
+        boolean isContinue = false;
         do {
             System.out.println("Hello");
             System.out.println("Continue? (1/0)");
-            int input = new Scanner(System.in).nextInt();
-            if (input == 0) {
-                isContinue = false;
-            }
 
         } while (isContinue);
     }
