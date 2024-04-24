3d8e1ff7c2250dd62cb44654ccf313af962f211c
telpvn <telpvn@mail.ru>
2024-04-24 09:40:49 +0400

    while

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 3; i <= 7; i++) {
-            System.out.println(i);
+
+        while (true) {
+            System.out.println("Hello");
         }
     }
 }
