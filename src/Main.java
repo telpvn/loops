afe0392614d555b35a1e37747f8de5f52cb1aa42
telpvn <telpvn@mail.ru>
2024-04-24 15:12:34 +0400

    Robot movement simulator

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean isContinue = false;
-        do {
-            System.out.println("Hello");
-            System.out.println("Continue? (1/0)");
-
-        } while (isContinue);
+        boolean isContinue = true;
+        int x = 0;
+        int y = 0;
+        while (isContinue) {
+            System.out.println("Выберите куда двигаться:\n1 - вверх\n2 - вниз\n3 - вправо\n4 - влево ");
+            int input = new Scanner(System.in).nextInt();
+            if (input == 1) {
+                y++;
+            } else if (input == 2) {
+                y--;
+            } else if (input == 3) {
+                x++;
+            } else if (input == 4) {
+                x--;
+            } else {
+                System.out.println("Error");
+                isContinue = false;
+            }
+            System.out.println("Координаты робота: по вертикали " + y + ", по горизонтали " + x);
+        }
     }
 }
