598b59c5acc3504dcfa12754d606fbd9f5b6012c
telpvn <telpvn@mail.ru>
2024-04-24 10:06:45 +0400

    while 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
-        while (true) {
+        boolean isContinue = true;
+        while (isContinue) {
             System.out.println("Hello");
+            System.out.println("Continue? (1/0)");
+            int input = new Scanner(System.in).nextInt();
+            if (input == 0) {
+                isContinue = false;
+            }
+
         }
     }
 }
