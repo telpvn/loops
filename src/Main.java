b214e89b8bb71e286825e2dc595bd91ab3e6f7c5
telpvn <telpvn@mail.ru>
2024-04-24 09:37:56 +0400

    Regular for loop 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            if (i < 3 || i > 7) {
-                continue;
-            }
-            System.out.println("Hello " + i);
+        for (int i = 3; i <= 7; i++) {
+            System.out.println(i);
         }
     }
 }
